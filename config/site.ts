44a522778a5ba06841944eca0cdfefa163cfc860
Leo Constantin <lcon69184@gmail.com>
2025-11-12 10:03:06 +0200

    chnaged site url in config

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "FoodWagen Site",
-  url: "https://beta-flow.vercel.app",
-  ogImage: "https://beta-flow.vercel.app/opengraph-image.png",
+  url: "https://foodwagen-2025.vercel.app/",
+  ogImage: "https://foodwagen-2025.vercel.app/opengraph-image.png",
   description: "Within a few clicks, find meals that are accessible near you",
 };
